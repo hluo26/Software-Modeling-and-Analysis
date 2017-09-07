8feb22febcf549043881ffc83d93e99e045eb649
hluo26 <h965l741@ku.edu>
2017-09-06 22:55:05 -0500

    Add simpl. for each proof

--- Project0/Project0.v
+++ Project0/Project0.v
@@ -52,78 +52,78 @@ Proof. simpl. reflexivity. Qed.
 Theorem symmetric_myOr: forall x y, myOr x y = myOr y x.
 intros x y. destruct x.
 destruct y.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 destruct y.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 Qed.
 
 Theorem symmetric_myAnd : forall x y, myAnd x y = myAnd y x.
 intros x y. destruct x.
 destruct y.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 destruct y.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 Qed.
 
 Theorem left_identity_or : forall x, myOr ffalse x = x.
 intros x. destruct x.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 Qed.
 
 Theorem left_idnetity_and : forall x, myAnd ttrue x = x.
 intros x. destruct x.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 Qed.
   
 Theorem not_not: forall x, myNot (myNot x) = x.
 intros x. destruct x.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 Qed.
   
 Theorem demorgan: forall x y, myNot (myAnd x y) = myOr (myNot x) (myNot y).
 intros x y. destruct x.
 destruct y.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 destruct y.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 Qed.
 
 Theorem contrapostive: forall x y, 
   (myImplies x y) = (myImplies (myNot y) (myNot x)).
 intros x y. destruct x.
 destruct y.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 destruct y.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 Qed.
 
 Theorem and_assoc: forall x y z, myAnd x (myAnd y z) = (myAnd (myAnd x y) z).
 intros x y z. destruct x.
 destruct y.
 destruct z.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 destruct z.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 destruct y.
 destruct z.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 destruct z.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 Qed.
 
 
@@ -131,18 +131,18 @@ Theorem and_distr: forall x y z, myAnd x (myOr y z) = (myOr (myAnd x y) (myAnd x
 intros x y z. destruct x.
 destruct y.
 destruct z.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 destruct z.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 destruct y.
 destruct z.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 destruct z.
-reflexivity.
-reflexivity.
+simpl. reflexivity.
+simpl. reflexivity.
 Qed.
 
 End Project0.
