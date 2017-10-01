39c28881197cbf553ed2a742e63cdbb5242208ab
hluo26 <h965l741@ku.edu>
2017-10-01 15:31:44 -0500

    Add 3 more proofs

--- Project2/Project2.v
+++ Project2/Project2.v
@@ -58,22 +58,70 @@ Module NatList.
   intros.
   induction l as [|n l'].
   reflexivity.
-
-  
+  simpl.
+  Admitted.
 
   Theorem app_assoc4 : forall l1 l2 l3 l4 : natlist,
   l1 ++ (l2 ++ (l3 ++ l4 )) = ((l1 ++ l2 ) ++ l3 ) ++ l4.
   Proof.
   Admitted.
 
+  Fixpoint beq_natlist (l1 l2 : natlist) : bool :=
+  match l1 with
+  |nil => match l2 with
+          | nil => true
+          | h2::l2' => false
+          end
+  |h1::l1' => match l2 with
+              | nil => false
+              | h2::l2' => if beq_nat h1 h2 then beq_natlist l1' l2' else false
+              end
+  end.
+
+  Example test_beq_natlist1 :
+  (beq_natlist nil nil = true).
+  Proof.
+  simpl. reflexivity.
+  Qed.
+
+  Example test_beq_natlist2 :
+  beq_natlist [1;2;3] [1;2;3] = true.
+  Proof.
+  simpl. reflexivity.
+  Qed.
+
+  Example test_beq_natlist3 :
+  beq_natlist [1;2;3] [1;2;4] = false.
+  Proof.
+  simpl. reflexivity.
+  Qed.
+
+  Lemma beq_nat_refl : forall n, true = beq_nat n n.
+  Proof.
+  intros n.
+  induction n.
+  simpl. reflexivity.
+  simpl. apply IHn.
+  Qed.
+  
+
+  Theorem beq_natlist_refl : forall l:natlist,
+  true = beq_natlist l l.
+  Proof.
+  intros l.
+  induction l as [|n l'].
+  simpl. reflexivity.
+  simpl. rewrite <- beq_nat_refl. 
+  rewrite <- IHl'. reflexivity.
+  Qed.
+  
+  
+  
+
   Lemma nonzeros_app : forall l1 l2 : natlist,
   nonzeros (l1 ++ l2 ) = (nonzeros l1 ) ++ (nonzeros l2 ).
   Proof.
   Admitted.
-
-  Fixpoint beq_natlist (l1 l2 : natlist) : bool :=
-  admit.
-  
   
   
 
